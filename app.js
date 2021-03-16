49d094eb73adccc12bd90c783e4019eb29bab016
Frayni Calderon <frayni10calderon@gmail.com>
2021-03-16 08:06:41 -0400

    Finish the responsive design

---
+++ app.js
@@ -0,0 +1,9 @@
+// Menu toggle
+const burgerMenu = document.getElementById("burger");
+const navbarMenu = document.getElementById("menu");
+
+// Responsive Navbar Toggle
+burgerMenu.addEventListener("click", function () {
+	navbarMenu.classList.toggle("active");
+	burgerMenu.classList.toggle("active");
+});
