d6b8bb0754cdcc3b28b97b8b06d064d4e63742f3
Frayni Calderon <frayni10calderon@gmail.com>
2021-03-16 10:20:13 -0400

    Adding modal + fix some problems

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ burgerMenu.addEventListener("click", function () {
 	navbarMenu.classList.toggle("active");
 	burgerMenu.classList.toggle("active");
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modal-close")[0];
+// Use with onclick="functionName" in the tag element
+function openModal() {
+	modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
